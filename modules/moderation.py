d232876d55f1bc2815a0dc43f83d33526cb5fab7
Ashton <ashton.nelson98@gmail.com>
2019-07-29 02:02:34 -0500

    Added mute commands

--- modules/moderation.py
+++ modules/moderation.py
@@ -5,7 +5,7 @@ import datetime
 import time
 import typing
 
-class Moderation(commands.Cog):
+class Moderation(commands.Cog, name = "Moderation"):
     """Server moderation commands"""
 
     def __init__(self, client):
@@ -19,7 +19,7 @@ class Moderation(commands.Cog):
         await guild.kick(user, reason = reason)
 
     @commands.command("ban")
-    async def ban(self, ctx, user: discord.User, *, reason: typing.Optional[str] = None):
+    async def ban(self, ctx, user: discord.Member, *, reason: typing.Optional[str] = None):
         """Ban given user from the server"""
         guild = ctx.guild
         await guild.ban(user, reason = reason)
@@ -31,11 +31,31 @@ class Moderation(commands.Cog):
         await guild.unban(user, reason = reason)
 
     @commands.command("softban")
-    async def softban(self, ctx, user: discord.User, *, reason: typing.Optional[str] = None):
+    async def softban(self, ctx, user: discord.Member, *, reason: typing.Optional[str] = None):
         """Ban and immediately unban given user from the server"""
         guild = ctx.guild
         await guild.ban(user, reason = "SOFTBAN: {0}".format(reason))
         await guild.unban(user, reason = "SOFTBAN: {0}".format(reason))
+
+    @commands.group("mute")
+    async def mute(self, ctx):
+        if (ctx.invoked_subcommand is None): # No subcommand implementation
+            await ctx.send("Please use !mute voice or !mute text")
+        
+    @mute.command("voice") #mute voice
+    async def mutevoice(self, ctx, user: discord.Member, *, reason: typing.Optional[str] = None):
+        """Prevent given user from speaking in voice channels"""
+        await user.edit(mute = True, reason = reason)
+
+    @mute.command("text") #mute text
+    async def mutetext(self, ctx, user: discord.Member, *, reason: typing.Optional[str] = None):
+        """Prevent given user from typing in text channels"""
+        await ctx.send("mute text")
+
+    @commands.command("unmute")
+    async def unmute(self, ctx, user: discord.Member):
+        """Unmute given user and restore speaking and typing capabilities"""
+        await user.edit(mute = False)
 
     @commands.command("banlist")
     async def banlist(self, ctx):
@@ -55,10 +75,7 @@ class Moderation(commands.Cog):
     @commands.command("clear")
     async def clear(self, ctx):
         """Send a blank message to clear chat"""
-        blank = ""
-        for i in range(35):
-            blank += "\n\u200B"
-
+        blank = "\n\u200B" * 50
         await ctx.send(blank + "`Chat cleared by {0}`".format(ctx.author))
 
 def setup(client):
