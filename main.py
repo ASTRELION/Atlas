92fb6a997a16b3a73cbc15f0812430a73efb83e2
Ashton <ashton.nelson98@gmail.com>
2019-07-30 23:18:51 -0500

    Created custom event loop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
-from discord.ext import commands
+from discord.ext import tasks, commands
+import asyncio
+import logging
 import json
 import time
 from collections import namedtuple
@@ -8,18 +10,19 @@ class AtlasClient(commands.Bot):
     """Custom Atlas Client inheriting Bot"""
 
     def __init__(self):
+        # Load Configuration
         self.config = {}
         with open("./config.json") as json_config:
             self.config = json.load(json_config)
-
+        
         # Init bot
         super().__init__(
             command_prefix = self.config["prefix"], 
             activity = discord.Game(self.config["activity"])
         )
 
-        self.remove_command("help") # Allow custom !help command
-
+        # Allow custom !help command
+        self.remove_command("help")
         # Used for embed message color
         self.color = discord.Color.from_rgb(
             self.config["color"][0],
@@ -27,10 +30,13 @@ class AtlasClient(commands.Bot):
             self.config["color"][2]
         )
 
+        # Init bot stats
         botStats = namedtuple("botStats", "start_time commands_processed")
         botStats.start_time = time.time()
         botStats.commands_processed = 0
         self.botStats = botStats
+
+        logging.basicConfig(level = logging.WARNING)
 
     # Fires when bot logs in
     async def on_connect(self):
@@ -55,13 +61,31 @@ class AtlasClient(commands.Bot):
     # Fires everytime a message is sent
     async def on_message(self, message):
         """Process command if valid"""
-        if (message.author == client.user): # Ignore messages sent by itself
+        if (message.author == client.user or 
+            not message.content.startswith(self.config["prefix"])): # Ignore messages sent by itself
             return
 
         # Handle command using modules
-        await self.process_commands(message)
+        # await self.process_commands(message)
+        ctx = await self.get_context(message, cls = commands.Context)
+        if (ctx.valid): await self.invoke(ctx)
+
         self.botStats.commands_processed += 1
 
+    async def on_command_error(self, ctx, exception):
+        """Handle error"""
+        print(exception)
+
 # Connect & Login
-client = AtlasClient()
-client.run(client.config["token"], bot = True, reconnect = True)
+try:
+    # client.run(client.config["token"], bot = True, reconnect = True)
+    client = AtlasClient()
+    client.loop.run_until_complete(client.start(
+        client.config["token"], 
+        bot = True, 
+        reconnect = True)
+    )
+except KeyboardInterrupt:
+    client.loop.run_until_complete(client.logout())
+finally:
+    client.loop.close()
