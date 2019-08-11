8b34b2dc69a24e00334c15a861121257cc304006
Ashton <ashton.nelson98@gmail.com>
2019-08-11 14:41:22 -0500

    Reload can reload all extensions, added load command

--- modules/admin.py
+++ modules/admin.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import json
+import typing
 
 class Admin(commands.Cog):
     """Administrative commands to be used by Bot and Server owners"""
@@ -19,13 +20,27 @@ class Admin(commands.Cog):
 
     @commands.command("reload")
     @commands.is_owner()
-    async def reload(self, ctx, *, module: str):
-        """Reload a module"""
-        module = str.lower(module)
-        self.client.reload_extension("modules.{0}".format(module))
-        await ctx.send("{0} module reloaded.".format(module))
+    async def reload(self, ctx, *, extension: typing.Optional[str] = None):
+        """Reload an extension or all extensions"""
+        if (extension is None): # Reload all extensions
+            for ext in self.config["extensions"]:
+                self.client.reload_extension(ext)
+            await ctx.send("All extensions reloaded.")
+        else: # Reload single extension
+            extension = str.lower(extension)
+            self.client.reload_extension(extension)
+            await ctx.send("{0} extension reloaded.".format(extension))
+
+    @commands.command("load")
+    @commands.is_owner()
+    async def load(self, ctx, *, extension: str):
+        """Load a new extension"""
+        extension = str.lower(extension)
+        self.client.load_extension(extension)
+        await ctx.send("{0} extension loaded.".format(extension))
 
     @commands.command("users")
+    @commands.guild_only()
     @commands.has_permissions(administrator = True)
     async def users(self, ctx):
         """List all users in the guild file associated with this server"""
